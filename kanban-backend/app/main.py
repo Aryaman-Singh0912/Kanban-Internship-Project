9a1b65a00bb894c7eff158e1aae1d02b688732cc
Aryaman Singh <stopdropandtroll0912@gmail.com>
2026-07-06 12:46:52 +0530

    Initialise auth and schemas

--- kanban-backend/app/main.py
+++ kanban-backend/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.routers import auth
+
 
 app = FastAPI()
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
+
+@app.get("/test-db")
+def test_db(db: Session = Depends(get_db)):
+    return {"message" : "database connection works"}
 
 @app.get("/")
 def read_root():
